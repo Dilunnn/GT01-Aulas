1a7e4e82c4d4cb1c2d4524003fc8487571a6c5fa
Dilunnn <rodrigoviana2203@gmail.com>
2025-05-15 12:13:06 -0300

    fim da aula30

---
+++ GT01/Modulo 1/Aulas/Aula30/desafioperfil/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Perfil from './Components/Perfil'
+
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className='text-center'>Perfil do Freelancer</h1>
+      <Perfil/>
+    </>
+  )
+}
+
+export default App
