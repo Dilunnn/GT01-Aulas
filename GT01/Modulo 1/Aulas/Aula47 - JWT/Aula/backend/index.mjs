b334f91f747a525d90c87c222ec7006ce9cd81f6
Dilunnn <rodrigoviana2203@gmail.com>
2025-06-09 11:56:19 -0300

    aula47

--- GT01/Modulo 1/Aulas/Aula47 - JWT/Aula/backend/index.mjs
+++ GT01/Modulo 1/Aulas/Aula47 - JWT/Aula/backend/index.mjs
@@ -7,6 +7,7 @@ npm install mysql2
 npm install --save-dev nodemon
 npm install dotenv
 npm install bcrypt
+npm install cors
 npm init -y
 editar o package.json:
 "start":"node index.mjs",
@@ -14,3 +15,52 @@ editar o package.json:
 */
 import express from 'express'
 import cors from 'cors'
+import mysql from 'mysql2'
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+const porta = 3000
+
+// Conexão com o MySql
+const conexao = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'1234',
+    database:'sistema_gestao_escolar'
+})
+
+// Rotas com banco de dados
+conexao.connect((erro) => {
+    if(erro){
+        console.log(`Erro ao conectar com o banco: ${erro}`)
+    }else{
+        console.log('Banco conectado com sucesso!')
+    }
+})
+
+app.get('/',(req,res) => {
+    res.send('Servidor rodando')
+})
+
+app.post('/verificarlogin', (req,res) => {
+    let usuario = req.body
+    let sql = `SELECT * FROM usuarios WHERE email = '${usuario.email}' and senha = '${usuario.senha}'`
+    conexao.query(sql,(er,result) => {
+        if (er) {
+            console.log(er);
+            return res.status(500).json({erro: 'Erro no servidor'})
+        }
+        if(result.length > 0){
+            result = result[0]
+            console.log(result); 
+        }
+    })
+    
+})
+
+
+app.listen(porta, ()=>{
+    console.log(`O servidor está rodando na porta ${porta}`)
+})
--- GT01/Modulo 1/Aulas/Aula47 - JWT/Aula/frontend/src/pages/Home.jsx
+++ GT01/Modulo 1/Aulas/Aula47 - JWT/Aula/frontend/src/pages/Home.jsx
@@ -1,67 +1,44 @@
-import React, { useState } from "react";
+import React, { useState } from 'react'
 
 const Home = () => {
-  let [email, setemail] = useState();
-  let [senha, setSenha] = useState();
+  let [email, setEmail] = useState()
+  let [senha, setSenha] = useState()
 
-  const fazerlogin = async (e) => {
+  const fazerLogin = async (e) => {
     e.preventDefault();
     try {
-      const resposta = await fetch("htttp://localhost:3000/verificarlogin", {
-        method: "post",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ email: email, senha: senha }),
-      });
-      const dados = await resposta.json();
+
+      const resposta = await fetch('http://localhost:3000/verificarlogin', { method: 'post', headers: { "Content-Type": "application/json" }, body: JSON.stringify({ email: email, senha: senha }) })
+      const dados = await resposta.json()
+      console(dados)
     } catch (error) {
-      console.log(error);
-      
+
     }
-  };
+  }
 
   return (
     <>
-      <div className="container-fluid vh-100 d-flex justify-content-center align-items-center">
-        <div className="card p-4">
-          <h2>Login:</h2>
-          <form onSubmit={fazerlogin}>
-            <div className="mb-3 ">
-              <label htmlFor="email" className="form-label">
-                E-mail:
-              </label>
-              <input
-                type="email"
-                id="username"
-                name="email"
-                className="form-control"
-                onChange={(e) => {
-                  setemail(e.target.value);
-                }}
-              />
+      <div className='container vh-100 d-flex justify-content-center align-items-center'>
+        <div className='card p-4' style={{ width: '400px' }}>
+          <h2 className='mb-4 text-center'>Login</h2>
+          <form onSubmit={fazerLogin}>
+            <div className='mb-3'>
+              <label className='form-label' htmlFor="email">E-mail:</label>
+              <input onChange={(e) => setEmail(e.target.value)} className='form-control' type="email" id='username' name='email' />
             </div>
-            <div className="mb-3">
-              <label htmlFor="senha" className="form-label">
-                Senha:
-              </label>
-              <input
-                type="password"
-                id="senha"
-                name="senha"
-                className="form-control"
-                onChange={(e) => {
-                  setSenha(e.target.value);
-                }}
-              />{" "}
-              <br />
-              <button type="submit" className="btn btn-secondary w-100">
-                Entrar
-              </button>
+
+            <div className='mb-3'>
+              <label className='form-label' htmlFor="senha">Senha:</label>
+              <input onChange={(e) => setSenha(e.target.value)} className='form-control' type="password" id='senha' name='senha' />
             </div>
+            <button className='btn btn-secondary w-100' type='submit'>Entrar</button>
           </form>
         </div>
       </div>
+      {email}
+      {senha}
     </>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
