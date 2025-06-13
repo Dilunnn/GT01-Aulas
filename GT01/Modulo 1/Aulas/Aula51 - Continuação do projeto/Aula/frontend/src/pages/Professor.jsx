805a245f0532d320c53f8adf7aaf63f15b4c5f90
Dilunnn <rodrigoviana2203@gmail.com>
2025-06-13 08:57:21 -0300

    inicio da aula 51

---
+++ GT01/Modulo 1/Aulas/Aula51 - Continuação do projeto/Aula/frontend/src/pages/Professor.jsx
@@ -0,0 +1,141 @@
+import React, { useEffect, useState } from 'react'
+
+const Professor = () => {
+
+    const professor = JSON.parse(localStorage.getItem('usuario'))
+    console.log(professor)
+
+    const [turmas, setTurmas] = useState([])
+    const [turma_id, setTurma_id] = useState("")           
+    const [turmaSelecionada, setTurmaSelecionada] = useState(false) 
+    const [tela, setTela] = useState()
+    const [alunos, setAlunos] = useState()
+
+    useEffect(() => {
+      CarregarTurmas()
+    },[])
+
+    console.log(tela)
+
+    async function CarregarTurmas() {
+      const id_prof = JSON.parse(localStorage.getItem('usuario')).id
+      const resp = await fetch(`http://localhost:3000/listarturmas/${id_prof}`)
+      const turmas = await resp.json()
+      console.log(turmas);
+      setTurmas(turmas)
+      
+
+    }
+
+    async function lancarNotas(){
+      const id_prof = JSON.parse(localStorage.getItem('usuario')).id
+      console.log(id_prof)
+      const resp = await fetch(`http://localhost:3000/trazerAlunos/${id_prof}/${turma_id}`)
+
+      const dados = await resp.json()
+
+      console.log(dados)
+      setAlunos(dados)
+      setTela('notas')
+    }
+
+    
+
+
+
+
+  return (
+  
+    <div>
+        <h1 className='text-center'>Painel do Professor</h1>
+        <div className='text-center mt-5'>
+
+          <h3>Selecione Sua turma</h3>
+          
+
+          <select onChange={e => setTurma_id(e.target.value)} value={turma_id}>
+            <option value="">--Selecione a turma--</option>
+            {turmas.map(turma => (
+              <option key={turma.id} value={turma.id}>{turma.nome}</option>
+            ))}
+          </select>
+          <br /><br />
+
+          <button onClick={() => setTurmaSelecionada(true)} className="btn btn-dark" disabled={!turma_id}> Selecionar </button>
+
+          <br />
+          <br />
+
+          {turmaSelecionada && (<div>
+              <button onClick={lancarNotas} className='btn btn-primary me-4'>Lançar Notas</button>
+
+              <button onClick={()=>setTela('freq')} className='btn btn-success'>Lançar Frequência</button>
+            </div>
+          )}
+
+          
+          
+        </div>
+        {!tela && 
+        (<div className='text-center mt-4'>
+          Selecione um opção acima
+        </div>)
+        }
+
+        
+
+        {tela == 'freq' && (
+          <div style={{width:'70%',margin:'10px auto'}}>
+            <h2>Lançamento de frequencia</h2>
+
+            <div className='d-flex p-4'>
+              <div style={{width:'300px'}}>
+              <span>João Silva</span>
+            </div>
+
+            <div>
+              <select name="" id="">
+                <option value="">Presente</option>
+                <option value="">Falta</option>
+              </select>
+            </div>
+            </div>
+            <hr />
+            
+            <br />
+            <button className='btn btn-success w-100'>Salvar Frequência</button>
+            
+            
+          </div>
+        )}
+
+        {tela == 'notas' && (
+          <div style={{width:'70%',margin:'10px auto'}}>
+            <h2>Lançamento de notas</h2>
+
+            {alunos.map((usuario) =>(
+                <div className='d-flex p-4'>
+                <div style={{width:'300px'}}>
+                  <span>{usuario.nome}</span>
+                </div>
+                <div>
+                  <span>Nota: </span>
+                  <input type="number" min={0} max={10} step={0.1}/>
+                </div>
+              </div>
+
+            ))}
+
+
+            
+            <button className='btn btn-primary w-100'>Salvar Frequência</button>
+          </div>
+        )}
+        
+
+
+    </div>
+  )
+}
+
+export default Professor
